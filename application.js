4d05536642226902ba30e3e94f26dfc905d50d69
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-10-05 13:00:18 -0400

    Yuba Sutter Mall 1.3  /application.js

--- application.js
+++ application.js
@@ -529,10 +529,192 @@ function renderHomeHours(container, template, collection){
         } else {
             val.h = "";
             $("#open_today").text("Closed today");
-            $('.hours_dot').css("background", "#FF001A");
+            $('.hours').css("background", "#FF001A");
         }
         var rendered = Mustache.render(template_html,val);
         item_rendered.push(rendered);
     });
+    $(container).html(item_rendered.join(''));
+}
+
+function renderHours(container, template, collection, type){
+    var item_list = [];
+    var item_rendered = [];
+    var template_html = $(template).html();
+    Mustache.parse(template_html);   // optional, speeds up future uses
+    if (type == "property_details"){
+        item_list.push(collection);
+        collection = []
+        collection = item_list;
+    }
+    if (type == "reg_hours") {
+        $.each( collection , function( key, val ) {
+            if (!val.store_id && val.is_holiday == false) {
+                switch(val.day_of_week) {
+                case 0:
+                    val.day = "Sunday"
+                    break;
+                case 1:
+                    val.day = "Monday"
+                    break;
+                case 2:
+                    val.day = "Tuesday"
+                    break;
+                case 3:
+                    val.day = "Wednesday"
+                    break;
+                case 4:
+                    val.day = "Thursday"
+                    break;
+                case 5:
+                    val.day = "Friday"
+                    break;
+                case 6:
+                    val.day = "Saturday"
+                    break;
+            }
+            if (val.open_time && val.close_time && val.is_closed == false){
+                // var open_time = new Date (val.open_time)
+                // var close_time = new Date (val.close_time)
+                // val.open_time = convert_hour(open_time);
+                // val.close_time = convert_hour(close_time);    
+                // val.h = val.open_time+ " - " + val.close_time;
+                
+                var open_time = moment(val.open_time).tz(getPropertyTimeZone());
+                
+                var close_time = moment(val.close_time).tz(getPropertyTimeZone());
+                
+                val.h = open_time.format("h:mm A") + " - " + close_time.format("h:mm A");
+            } else {
+                "Closed"
+            }
+                item_list.push(val)
+            }
+        });
+        collection = []
+        collection = item_list;
+    }
+    // if (type == "holiday_hours") {
+    //     $.each( collection , function( key, val ) {
+    //         if (!val.store_id && val.is_holiday == true) {
+    //             holiday = new Date (val.holiday_date)
+    //             holiday.setDate(holiday.getDate()+1);
+    //             var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    //             val.formatted_date = weekdays[holiday.getDay()]+ " " + get_month(holiday.getMonth()) + " " +holiday.getDate()+ " " + holiday.getFullYear()
+    //             if (val.open_time && val.close_time && val.is_closed == false){
+    //                 var open_time = new Date (val.open_time)
+    //                 var close_time = new Date (val.close_time)
+    //                 val.open_time = convert_hour(open_time);
+    //                 val.close_time = convert_hour(close_time);    
+    //                 if (val.open_time == "0:00 AM"){
+    //                     val.open_time = "12:00 AM"
+    //                 }
+    //                  if (val.close_time == "0:00 AM"){
+    //                     val.close_time = "12:00 AM"
+    //                 }
+    //                 val.h = val.open_time+ " - " + val.close_time;
+    //             } else {
+    //                 val.h = "Closed"
+    //             }
+    //             if (val.h != "Closed"){
+    //                 item_list.push(val)
+    //             }
+    //         }
+    //     });
+    //     collection = []
+    //     collection = item_list;
+    // }
+    
+    if (type == "holiday_hours") {
+        $.each( collection , function( key, val ) {
+            if (!val.store_id && val.is_holiday == true) {
+                holiday = moment(val.holiday_date).tz(getPropertyTimeZone());
+                var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+                val.formatted_date = holiday.format("dddd MMM D YYYY");
+                
+                if (val.open_time && val.close_time && val.is_closed == false){
+                    var open_time = moment(val.open_time).tz(getPropertyTimeZone());
+                    var close_time = moment(val.close_time).tz(getPropertyTimeZone());
+                    if (val.open_time == "0:00 AM"){
+                        val.open_time = "12:00 AM"
+                    }
+                     if (val.close_time == "0:00 AM"){
+                        val.close_time = "12:00 AM"
+                    }
+                    val.h = open_time.format("h:mmA") + " - " + close_time.format("h:mmA");
+                } else {
+                    val.h = "Closed"
+                }
+                if (val.h != "Closed"){
+                    item_list.push(val)
+                }
+            }
+        });
+        collection = []
+        collection = item_list;
+    }
+    // if (type == "closed_hours") {
+    //     $.each( collection , function( key, val ) {
+    //         if (!val.store_id && val.is_holiday == true) {
+    //             holiday = new Date (val.holiday_date + "T05:00:00Z")
+    //             var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    //             val.formatted_date = weekdays[holiday.getDay()]+ " " + get_month(holiday.getMonth()) + " " +holiday.getDate()+ " " + holiday.getFullYear()
+    //             if (val.open_time && val.close_time && val.is_closed == false){
+    //                 var open_time = new Date (val.open_time)
+    //                 var close_time = new Date (val.close_time)
+    //                 val.open_time = convert_hour(open_time);
+    //                 val.close_time = convert_hour(close_time);    
+    //                 if (val.open_time == "0:00 AM"){
+    //                     val.open_time = "12:00 AM"
+    //                 }
+    //                  if (val.close_time == "0:00 AM"){
+    //                     val.close_time = "12:00 AM"
+    //                 }
+    //                 val.h = val.open_time+ " to " + val.close_time;
+    //             } else {
+    //                 val.h = "Closed"
+    //             }
+    //             if (val.h == "Closed"){
+    //                 item_list.push(val)
+    //             }
+    //         }
+    //     });
+    //     collection = []
+    //     collection = item_list;
+    // }
+    
+    if (type == "closed_hours") {
+        $.each( collection , function( key, val ) {
+            if (!val.store_id && val.is_holiday == true) {
+                holiday = moment(val.holiday_date).tz(getPropertyTimeZone());
+                var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+                val.formatted_date = holiday.format("dddd MMM D YYYY");
+                
+                if (val.open_time && val.close_time && val.is_closed == false){
+                    var open_time = moment(val.open_time).tz(getPropertyTimeZone());
+                    var close_time = moment(val.close_time).tz(getPropertyTimeZone());
+                    if (val.open_time == "0:00 AM"){
+                        val.open_time = "12:00 AM"
+                    }
+                     if (val.close_time == "0:00 AM"){
+                        val.close_time = "12:00 AM"
+                    }
+                    val.h = open_time.format("h:mm A") + " to " + close_time.format("h:mm A");
+                } else {
+                    val.h = "Closed"
+                }
+                if (val.h == "Closed") {
+                    item_list.push(val)
+                }
+            }
+        });
+        collection = []
+        collection = item_list;
+    }
+    $.each( collection , function( key, val ) {
+        var rendered = Mustache.render(template_html,val);
+        item_rendered.push(rendered);
+    });
+    $(container).show();
     $(container).html(item_rendered.join(''));
 }
