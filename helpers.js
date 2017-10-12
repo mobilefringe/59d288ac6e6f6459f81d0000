42e032e0e80c6c6323a156a014b6e8bdefefc5a9
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-10-12 12:08:47 -0400

    Yuba Sutter Mall 1.3  /helpers.js

--- helpers.js
+++ helpers.js
@@ -113,6 +113,7 @@ function show_cat_stores(){
         });
         // $(".col-md-8").addClass("one_column");
         console.log($("#store_list_container").height());
+        $("#store_list_container").show();
         if($("#store_list_container").height() == 0){
             $("#store_list_container").hide();
             
