f9ad5cf7b1cd523162c294bc080ccf5fcf127fb6
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-10-12 12:34:40 -0400

    Yuba Sutter Mall 1.3  /site.js

--- site.js
+++ site.js
@@ -3,5 +3,5 @@ var site_json = {
                     "default_image" : "//codecloud.cdn.speedyrails.net/sites/59d288ac6e6f6459f81d0000/image/png/1507819329000/yb_logo.png",
                     "time_zone" : "US/Eastern",
                     // "newsletter_form_url" : "//mobilefringe.createsend.com/t/d/s/bkhoy/?callback=?",
-                    "newsletter_email_field" : "cm-bkhoy-bkhoy"
+                    // "newsletter_email_field" : "cm-bkhoy-bkhoy"
                 }
