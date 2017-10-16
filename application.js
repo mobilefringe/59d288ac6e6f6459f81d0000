4122bc086b300d6c3bb283055e463682b844f2f2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-10-16 13:33:16 -0400

    Yuba Sutter Mall 1.3  /application.js

--- application.js
+++ application.js
@@ -58,8 +58,8 @@ function renderFeatureItems(container, template, collection){
     // });
     // $(container).html(item_rendered.join(''));
     console.log("hello")
-    var items = getFeatureList();
-    $.each(items, function(i, val){
+    // var items = getFeatureList();
+    $.each(collection, function(i, val){
         $('#feature_' + i).html('<a href="'+ val.url +'"><figure style="background-image:url('+ val.image_url+')"></figure> <div class="hover_see_more_btn"><h5 class="hover_text">'+ val.name +'</h5></div></a>');
         // <a href="{{url}}">
                         // <figure style="background-image:url('{{image_url}}')"></figure>
