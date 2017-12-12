394c88244d29470c3a233f2b9d8f2c16978b3ff2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-12 11:05:34 -0500

    Yuba Sutter Mall 1.3  /helpers.js

--- helpers.js
+++ helpers.js
@@ -87,6 +87,9 @@ function init(e){
         $('.hidden-popup-bg').hide()
         $('body').removeClass('no_scroll');
     });
+    //dynamically changing copyright year
+var current_year = moment().year();
+$("#current_year").text(current_year);
 }
 
 function show_content(){
