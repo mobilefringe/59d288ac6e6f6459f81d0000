dce2e21ad07fad7e82f1086d7d86d208a62ee5ef
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2017-12-13 14:31:34 -0500

    Yuba Sutter Mall 1.3  /helpers.js

--- helpers.js
+++ helpers.js
@@ -93,6 +93,7 @@ function init(e){
     $("#current_year").text(current_year);
     
     var sticky_offset = 100;
+    var navbar = $("navbar");
     window.onscroll = function() {
         if (window.pageYOffset >= sticky_offset) {
             navbar.classList.add("sticky")
