2c9774964f9ed26ce993e45b55414ac5237558f8
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-08-23 14:49:40 -0400

    Yuba Sutter Mall 1.4  /site.js

--- site.js
+++ site.js
@@ -1,7 +1,5 @@
 var site_json = {
-                    "mall_name" : "Yuba Sutter Mall",
-                    "default_image" : "//codecloud.cdn.speedyrails.net/sites/5b55f49f6e6f64715f010000/image/jpeg/1532366599109/yb_logo-default.jpg",
-                    "time_zone" : "US/Eastern",
-                    // "newsletter_form_url" : "//mobilefringe.createsend.com/t/d/s/bkhoy/?callback=?",
-                    // "newsletter_email_field" : "cm-bkhoy-bkhoy"
-                }
+    "mall_name" : "Yuba Sutter Mall",
+    "default_image" : "//codecloud.cdn.speedyrails.net/sites/5b55f49f6e6f64715f010000/image/jpeg/1532366599109/yb_logo-default.jpg",
+    "time_zone" : "US/Eastern",
+}
